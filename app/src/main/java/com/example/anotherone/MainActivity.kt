3812c8d0d8f34f31286d31595984743542d05108
Fsho666 <s1921152@cco.kanagawa-it.ac.jp>
2021-09-04 00:36:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/anotherone/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.anotherone
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+
+        //ボタンをクリックしたときに行う動作
+        rollButton.setOnClickListener {
+            //toastを表示
+            val toast = Toast.makeText(this, "OK!!", Toast.LENGTH_SHORT)
+            toast.show()
+            //ブラウザを立ち上げる
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://192.168.1.3:8080"))
+            startActivity(intent)
+        }
+    }
+}
